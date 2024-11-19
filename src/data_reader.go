73daa574e0a86bb2cc4bf9720ff18227cf5b0a7c
jenconn_p <ye1wyy@gmail.com>
2024-11-19 12:56:48 +0700

    develop: name change and add xml reader, but catched nil in output from xml reader

--- src/data_reader.go
+++ src/data_reader.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"encoding/xml"
 	"flag"
 	"fmt"
+	"io"
+	"os"
 )
 
 var (
@@ -13,8 +16,56 @@ type DBReader interface {
 	ExtractData()
 }
 
+type XmlData struct {
+	XMLName xml.Name `xml:"recipes"`
+	cake    []Cake   `xml:"cake"`
+}
+
+type Cake struct {
+	XMLName     xml.Name      `xml:"cake"`
+	name        string        `xml:"name"`
+	stovetime   string        `xml:"stovetime"`
+	ingredients []Ingredients `xml:"ingredients"`
+}
+
+type Ingredients struct {
+	XMLName xml.Name `xml:"ingredients"`
+	name    string   `xml:"itemname"`
+	count   float32  `xml:"itemcount"`
+	unit    string   `xml:"itemunit"`
+}
+
+type JsonData struct {
+	name, stove_time string
+	ingridients      []string
+}
+
+func (x XmlData) ExtractData() {
+	XmlFile, err := os.Open(*FFlag)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Printf("\nFile opened\n")
+	defer XmlFile.Close()
+
+	byteValue, _ := io.ReadAll(XmlFile)
+	err = xml.Unmarshal(byteValue, &x)
+
+	if err != nil {
+		fmt.Printf("[Error]: %v\n", err)
+		return
+	}
+
+	fmt.Printf("%#v", x)
+}
+
 func main() {
 	flag.Parse()
 
 	fmt.Print("File: ", *FFlag)
+	data := new(XmlData)
+	data.ExtractData()
 }
